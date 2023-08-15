d1163f00505008d6d945521719c5757022116c4b
ChristianBarradas <christian_barradas@outlook.com>
2023-08-14 23:28:50 -0600

    update files API

--- house_price_prediction/house_price_prediction/api/main.py
+++ house_price_prediction/house_price_prediction/api/main.py
@@ -1,41 +1,29 @@
 import os
 import sys
-from api import app
+
+from fastapi import FastAPI
+from predictor.predict import ModelPredictor
+from starlette.responses import JSONResponse
+
+from .models.models import HousePricePrediction
 
 # Add the parent directory to sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.append(parent_dir)
 
-from fastapi import FastAPI
-from starlette.responses import JSONResponse
-
-from predictor.predict import ModelPredictor
-from .models.models import Titanic
 
 app = FastAPI()
 
+
 @app.get('/', status_code=200)
 async def healthcheck():
-    return 'Titanic classifier is all ready to go!'
+    return 'house predict classifier is all ready to go!'
+
 
 @app.post('/predict')
-def extract_name(titanic_features: Titanic):
+def extract_name(housing_features: HousePricePrediction):
     predictor = ModelPredictor("/Users/SISTEMAS/MLOPS-PROJECT/house_price_prediction/house_price_prediction/models/neural_network_model.pkl")
-    X = [titanic_features.pclass_nan,
-        titanic_features.age_nan,
-        titanic_features.sibsp_nan,
-        titanic_features.parch_nan,
-        titanic_features.fare_nan,
-        titanic_features.sex_male,
-        titanic_features.cabin_Missing,
-        titanic_features.cabin_rare,
-        titanic_features.embarked_Q,
-        titanic_features.embarked_S,
-        titanic_features.title_Mr,
-        titanic_features.title_Mrs,
-        titanic_features.title_rar]
+    X = [housing_features.bedrooms, housing_features.bathrooms, housing_features.sqft_living, housing_features.sqft_lot, housing_features.floors, housing_features.waterfront, housing_features.view, housing_features.condition, housing_features.grade, housing_features.sqft_above, housing_features.sqft_basement, housing_features.yr_built, housing_features.yr_renovated, housing_features.lat, housing_features.long, housing_features.sqft_lot15, housing_features.sqft_living15, housing_features.month, housing_features.year]
     prediction = predictor.predict([X])
     return JSONResponse(f"Resultado predicción: {prediction}")
-
-
